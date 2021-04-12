bc64cd174a13729f13abf6679ebfb82d5eea15ad
Allan Amistoso <anamistoso@up.edu.ph>
2021-04-12 10:02:20 +0800

    initial commit

---
+++ humidity.ino
@@ -0,0 +1,31 @@
+#include <dht.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+
+dht DHT;
+
+#define DHT11_PIN 7
+
+void setup(){
+  Serial.begin(9600);
+
+  lcd.begin();                      // initialize the lcd 
+  // Print a message to the LCD.
+  lcd.backlight();
+}
+
+void loop(){
+  int chk = DHT.read11(DHT11_PIN);
+  lcd.setCursor(0,0); 
+  lcd.print("Temp: ");
+  lcd.print(DHT.temperature);
+  lcd.print((char)223);
+  lcd.print("C");
+  lcd.setCursor(0,1);
+  lcd.print("Humidity: ");
+  lcd.print(DHT.humidity);
+  lcd.print("%");
+  delay(1000);
+}
